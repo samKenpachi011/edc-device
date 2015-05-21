8081751242990e5a54c4e8d289e09c9c0f79ac91
erik van widenfelt <ew2789@gmail.com>
2015-05-21 15:28:13 +0200

    update setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='edc-device',
-    version='0.0.0a0',
+    version='0.1.0',
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
@@ -22,10 +22,6 @@ setup(
     long_description=README,
     zip_safe=False,
     keywords='django contenttype',
-    install_requires=[
-        'Django>=1.7',
-        'unipath>=1.1',
-    ],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
