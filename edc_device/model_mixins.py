935e20e4210e26fb867d3bd5c63ed006e84c8218
erikvw <ew2789@gmail.com>
2016-12-17 22:29:27 +0200

    add validation against device permissions in common_clean, add device_created, devie_modified field attrs

--- edc_device/model_mixins.py
+++ edc_device/model_mixins.py
@@ -5,13 +5,14 @@ from edc_device.exceptions import DevicePermissionError
 
 class DeviceModelMixin(models.Model):
     """Mixin to add device created and modified fields and check device permissions."""
+
     device_created = models.CharField(
         max_length=10,
-        editable=False)
+        blank=True)
 
     device_modified = models.CharField(
         max_length=10,
-        editable=False)
+        blank=True)
 
     def save(self, *args, **kwargs):
         app_config = django_apps.get_app_config('edc_device')
@@ -26,6 +27,17 @@ class DeviceModelMixin(models.Model):
             self.device_created = app_config.device_id
         self.device_modified = app_config.device_id
         super().save(*args, **kwargs)
+
+    def common_clean(self):
+        app_config = django_apps.get_app_config('edc_device')
+        if self.id:
+            if app_config.device_permissions.get(self._meta.label_lower):
+                device_permission = app_config.device_permissions.get(self._meta.label_lower)
+                if not device_permission.may_change(app_config.role):
+                    raise DevicePermissionError(
+                        'Device does not have permissions to change this model. Got role={}, model={}'.format(
+                            app_config.role, self._meta.label_lower))
+        super().common_clean()
 
     class Meta:
         abstract = True
