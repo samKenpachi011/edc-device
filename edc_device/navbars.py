a8598ee0c762fbb8ed70f3a057dc5bc3e0d4f76b
erikvw <ew2789@gmail.com>
2017-11-15 17:57:15 +0200

    update navbars

--- edc_device/navbars.py
+++ edc_device/navbars.py
@@ -1,12 +1,17 @@
+from django.conf import settings
 from edc_navbar import Navbar, NavbarItem, site_navbars
 
-
 device = Navbar(name='edc_device')
+
+if settings.APP_NAME == 'edc_device':
+    url_namespace = None
+else:
+    url_namespace = settings.APP_NAME
 
 device.append_item(
     NavbarItem(name='device',
                label='device',
                fa_icon='fa-calculator',
-               url_name='edc_device:home_url'))
+               url_name=':'.join(x for x in [url_namespace, 'home_url'] if x)))
 
 site_navbars.register(device)
