b2d1dd066eafad04eb471344fb72558c22225fd7
erikvw <ew2789@gmail.com>
2017-11-15 17:59:30 +0200

    add urls for tests

--- edc_device/urls.py
+++ edc_device/urls.py
@@ -1,9 +1,22 @@
-from django.conf.urls import url
-
+from django.urls.conf import path
 from edc_device.views import HomeView
+from edc_base.views import AdministrationView, LoginView, LogoutView
+from django.conf import settings
+
 
 app_name = 'edc_device'
 
-urlpatterns = [
-    url(r'^', HomeView.as_view(), name='home_url'),
+
+if settings.APP_NAME == 'edc_device':
+    urlpatterns = [
+        path('administration/', AdministrationView.as_view(),
+             name='administration_url'),
+        path('login', LoginView.as_view(), name='login_url'),
+        path('logout', LogoutView.as_view(
+            pattern_name='login_url'), name='logout_url')]
+else:
+    urlpatterns = []
+
+urlpatterns += [
+    path(r'', HomeView.as_view(), name='home_url'),
 ]
