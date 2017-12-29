d559ee4f3008acd1e2b9479bd47e852e36cf43d6
erikvw <ew2789@gmail.com>
2017-12-29 11:31:18 +0200

    update settings

--- edc_device/settings.py
+++ edc_device/settings.py
@@ -14,6 +14,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APP_NAME = 'edc_device'
+SITE_ID = 40
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'django_revision.apps.AppConfig',
     'edc_navbar.apps.AppConfig',
     'edc_base.apps.AppConfig',
