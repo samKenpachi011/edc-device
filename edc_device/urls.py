f2b34c16809da53a2228d6d52efeedd0d355f2c9
erikvw <ew2789@gmail.com>
2018-01-16 19:33:50 +0200

    update auth urls

--- edc_device/urls.py
+++ edc_device/urls.py
@@ -1,7 +1,7 @@
-from django.urls.conf import path, include
-from edc_device.views import HomeView
-from edc_base.views import AdministrationView, LoginView, LogoutView
 from django.conf import settings
+from django.urls.conf import path, include
+from edc_base.views import AdministrationView
+from edc_device.views import HomeView
 
 
 app_name = 'edc_device'
@@ -9,12 +9,10 @@ app_name = 'edc_device'
 
 if settings.APP_NAME == 'edc_device':
     urlpatterns = [
+        path('accounts/', include('edc_base.auth.urls')),
         path('edc_base/', include('edc_base.urls')),
         path('administration/', AdministrationView.as_view(),
-             name='administration_url'),
-        path('login', LoginView.as_view(), name='login_url'),
-        path('logout', LogoutView.as_view(
-            pattern_name='login_url'), name='logout_url')]
+             name='administration_url')]
 else:
     urlpatterns = []
 
